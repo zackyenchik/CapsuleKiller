9182f02f8f1c95d2143205074cf6a15079f029b4
Zack Yenchik <zcy3071@g.rit.edu>
2017-12-16 17:24:00 -0500

    Refined jumoing mechanics and imported some skyboxes for potential use later on.

--- Level_Design_Practice/Assets/BasicMove.cs
+++ Level_Design_Practice/Assets/BasicMove.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class BasicMove : MonoBehaviour {
 
     public float speed = 0.75f;
+    public float jumpforce;
+    public bool isgrounded = true;
 
 	// Use this for initialization
 	void Start () {
-		
+        jumpforce = 2f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        Vector3 position = transform.position;
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += new Vector3(0, 0, speed);
@@ -29,6 +32,17 @@ public class BasicMove : MonoBehaviour {
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Space) && isgrounded)
+        {
+            transform.position += new Vector3(0, jumpforce, 0);
+            speed = 0.35f;
+            isgrounded = false;
+        }
+        if (position.y <= 1.1f)
+        {
+            speed = 0.75f;
+            isgrounded = true;
         }
 	}
 }
