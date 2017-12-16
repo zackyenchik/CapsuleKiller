d826f5d42cbed96d6fa24cff6e474b0599fad1b5
Zack Yenchik <zcy3071@g.rit.edu>
2017-12-16 16:37:24 -0500

    First commit

---
+++ Level_Design_Practice/Assets/BasicMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMove : MonoBehaviour {
+
+    public float speed = 0.75f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+	}
+}
