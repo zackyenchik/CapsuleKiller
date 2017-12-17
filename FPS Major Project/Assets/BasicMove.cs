ac19b10a5e5654fc53d3ef9ead40f34e0e58c763
Zack Yenchik <zcy3071@g.rit.edu>
2017-12-16 19:24:19 -0500

    Added basic mouse look capabilitites and a wall.

---
+++ FPS Major Project/Assets/BasicMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMove : MonoBehaviour {
+
+    public float speed = 0.35f;
+    public float jumpforce;
+    private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+        jumpforce = 4f;
+        grounded = true;
+	}
+
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        Vector3 position = transform.position;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            transform.position += new Vector3(0, jumpforce, 0);
+            speed = 0.2f;
+            grounded = false;
+        }
+        if (position.y <= 1)
+        {
+            grounded = true;
+            speed = 0.35f;
+        }
+    }
+}
---
+++ FPS Major Project/Assets/MouseLook.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+    public float sensitivity = 4f;
+    public float xRotation;
+    public float yRotation;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        xRotation -= Input.GetAxis("Mouse Y") * sensitivity;
+        yRotation += Input.GetAxis("Mouse X") * sensitivity;
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+	}
+}
