b4f6f01bb9ec100e1cc5b40fe4a55ccf1c85a27c
Zack Yenchik <zcy3071@g.rit.edu>
2017-12-18 22:19:41 -0500

    Added an enemy spawn system that spawns an enemy in a random position after a certain amount of time.

--- FPS Major Project/Assets/PlayerHealth.cs
+++ FPS Major Project/Assets/PlayerHealth.cs
@@ -20,16 +20,16 @@ public class PlayerHealth : MonoBehaviour {
 	void Update () {
 		if (Vector3.Distance(transform.position, Enemy.position) <= mindistance && attackcooldown == 0f)
         {
-            currentHealth -= 1;
-            attackcooldown += 10f;
+            currentHealth -= 1; // Decrease player's health if the enemy is in range
+            attackcooldown += 10f; // Reset the attack cooldown for the enemy
         }
         if (attackcooldown > 0)
         {
-            attackcooldown -= 1;
+            attackcooldown -= 1; // Decrease the attack cooldown
         }
         if (currentHealth <= 0)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reload scene if the player's health reaches 0
         }
 	}
 }
