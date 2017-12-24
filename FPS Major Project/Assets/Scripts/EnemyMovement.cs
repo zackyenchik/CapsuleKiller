64141b7eb368c7e2f1c326abd1ffbc4b834fd242
Zack Yenchik <zcy3071@g.rit.edu>
2017-12-23 23:00:48 -0500

    Tweaked the EnemyMovement script to make enemies stop moving when they are at the distance required to do damage to the Player.

--- FPS Major Project/Assets/Scripts/EnemyMovement.cs
+++ FPS Major Project/Assets/Scripts/EnemyMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMoveTowardPlayer : MonoBehaviour {
+public class EnemyMovement : MonoBehaviour {
 
     public Transform Player;
     public float movespeed = 5f;
@@ -17,9 +17,13 @@ public class EnemyMoveTowardPlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(Player); // Look at the player
-        if (Vector3.Distance(transform.position, Player.position) >= mindistance) // If the enemy is less than the minimum distance away from the player
+        if (Vector3.Distance(transform.position, Player.position) >= mindistance) // If the enemy is greater than the minimum distance away from the player
         {
             transform.position += transform.forward * movespeed * Time.deltaTime; // Move towards the player
+        }
+        else
+        {
+            transform.position += transform.forward * 0 * Time.deltaTime;
         }
     }
 }
