dbb083e0e9ef366ee5b4632bf0faafd01daf6671
Zack Yenchik <zcy3071@g.rit.edu>
2018-01-09 00:23:30 -0500

    Added a start menu that works when the game is built and ran.

---
+++ FPS Major Project/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void StartButton(string newGameLvl)
+    {
+        SceneManager.LoadScene(newGameLvl);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
